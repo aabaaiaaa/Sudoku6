4d6c2a301f215d740bcb5e2cddedb50d89004655
Jay Eastaugh <aabaaiaaa@gmail.com>
2018-09-24 23:52:32 +0100

    Generation of rows functional
    Generation process validates each row only contains one of each number from 1 - 9.
    Next is to verify that each column only contains one of each number from 1 - 9.

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -4,8 +4,31 @@ import { PositionComponent } from "./position.component";
 @Injectable()
 export class GameService {
     generateNewPuzzle(arrayLength: number, numberRange: number): Array<number> {
+        let nextNumber = (existingNumber) => {
+            if (!existingNumber) return Math.ceil(Math.random() * numberRange);
+            var newNumber = existingNumber < numberRange ? ++existingNumber : 1
+            return newNumber;
+        }
+        let numberInRow = (numberToTest, index, array) => {
+            var row = Math.floor((index)/numberRange);
+            var numberInRowAlready = false;
+            for(let i = row*numberRange; i < (row*numberRange)+numberRange; i++){
+                if(i == index) continue;
+                numberInRowAlready = (array[i] == numberToTest);
+                if(numberInRowAlready) break;
+            }
+            return numberInRowAlready;
+        };
+
         console.log("Generate new puzzle");
-        return Array.from(new Array(arrayLength), () => Math.ceil(Math.random() * numberRange));
+        var puzzleArray = Array.apply(null, new Array<number>(arrayLength));
+        for(let i = 0; i < puzzleArray.length; i++){
+            var positionValue = puzzleArray[i];
+            var numberToTest = nextNumber(positionValue);
+            puzzleArray[i] = numberToTest;
+            if(numberInRow(numberToTest, i, puzzleArray)) i--; // try another number
+        }
+        return puzzleArray;
     }
     setSelectedPositionValue(positionValue: number) {
         this.selectedPosition.value = positionValue;
