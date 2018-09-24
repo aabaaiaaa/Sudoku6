96a0a12481052d4fe35b0acc3438df489798bd58
Jay Eastaugh <aabaaiaaa@gmail.com>
2018-09-25 00:50:09 +0100

    Row and column logic in.. but
    Looks like the logic isn't checking all rows and columns as it populates the position array. Will need to investigate.

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -4,29 +4,51 @@ import { PositionComponent } from "./position.component";
 @Injectable()
 export class GameService {
     generateNewPuzzle(arrayLength: number, numberRange: number): Array<number> {
-        let nextNumber = (existingNumber) => {
-            if (!existingNumber) return Math.ceil(Math.random() * numberRange);
+        let nextNumber = (existingNumber): number => {
+            if (!existingNumber) return 1;//Math.ceil(Math.random() * numberRange);
             var newNumber = existingNumber < numberRange ? ++existingNumber : 1
             return newNumber;
         }
-        let numberInRow = (numberToTest, index, array) => {
-            var row = Math.floor((index)/numberRange);
+        let numberInRow = (numberToTest, index, array): boolean => {
+            var row = Math.floor((index) / numberRange);
             var numberInRowAlready = false;
-            for(let i = row*numberRange; i < (row*numberRange)+numberRange; i++){
-                if(i == index) continue;
+            for (let i = row * numberRange; i < (row * numberRange) + numberRange; i++) {
+                if (i == index) continue;
                 numberInRowAlready = (array[i] == numberToTest);
-                if(numberInRowAlready) break;
+                if (numberInRowAlready) break;
             }
             return numberInRowAlready;
+        };
+        let numberInColumn = (numberToTest, index, array): boolean => {
+            var column = index % numberRange;
+            var numberInColumnAlready = false;
+            for (let i = column; i < column + 73; i = i + numberRange) {
+                if (i == index) continue;
+                numberInColumnAlready = (array[i] == numberToTest);
+                if (numberInColumnAlready) break;
+            }
+            return numberInColumnAlready;
         };
 
         console.log("Generate new puzzle");
         var puzzleArray = Array.apply(null, new Array<number>(arrayLength));
-        for(let i = 0; i < puzzleArray.length; i++){
-            var positionValue = puzzleArray[i];
-            var numberToTest = nextNumber(positionValue);
-            puzzleArray[i] = numberToTest;
-            if(numberInRow(numberToTest, i, puzzleArray)) i--; // try another number
+        for (let i = 0; i < puzzleArray.length; i++) {
+            var retries = 0;
+            while (retries < 10) {
+                retries++;
+                var positionValue = puzzleArray[i];
+                var numberToTest = nextNumber(positionValue);
+                puzzleArray[i] = numberToTest;
+                if (numberInRow(numberToTest, i, puzzleArray)){ 
+                    // try another number
+                    continue;
+                }
+                if (numberInColumn(numberToTest, i, puzzleArray)){
+                    // try another number
+                    continue;
+                }
+                retries = 10;
+            }
         }
         return puzzleArray;
     }
