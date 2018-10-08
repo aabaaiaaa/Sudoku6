e5f2424fc392a57b8d2a23a55e605ed4d8a5d667
Jay Eastaugh <aabaaiaaa@gmail.com>
2018-10-08 17:40:08 +0100

    Jest/Karma unit tests added
    To make sure the grid checking logic doesn't break the other row and column checking, tests are being utilised.

---
+++ src/app/game.service.spec.ts
@@ -0,0 +1,77 @@
+import { GameService } from "./game.service";
+
+describe('game service', () => {
+    let gameService = new GameService();
+
+    describe('next number funcitonality', () => {
+        let nextNumber = gameService.nextNumber(9);
+
+        it('should return the first valid number to try', () => {
+            expect(nextNumber(undefined)).toBe(1);
+        });
+        it('should return the next valid number to try', () => {
+            expect(nextNumber(1)).toBe(2);
+        });
+        it('should return the last valid number to try', () => {
+            expect(nextNumber(8)).toBe(9);
+        });
+        it('should loop back to the first valid number to try', () => {
+            expect(nextNumber(9)).toBe(1);
+        });
+    });
+
+    describe('number is in row functionality', () => {
+        let numberInRow = gameService.numberInRow(9);
+
+        it("shouldn't find the number in row", () => {
+            expect(numberInRow(1, 0, [2, 3, 4])).toBe(false);
+        })
+
+        it('should find the number in row', () => {
+            let actualValue = numberInRow(2, 0, [1, 2, 3]);
+            expect(actualValue).toBe(true);
+        })
+
+        it('should ignore the indexed number in row', () => {
+            let actualValue = numberInRow(2, 1, [1, 2, 3]);
+            expect(actualValue).toBe(false);
+        })
+    });
+
+    describe('number is in column functionality', () => {
+        let numberInColumn = gameService.numberInColumn(9);
+
+        it("shouldn't find the number in column", () => {
+            expect(numberInColumn(1, 0, [2, 3, 4])).toBe(false);
+            expect(numberInColumn(3, 1,
+                [2, 3, 4, 5, 6, 7, 8, 9, 1]
+                    .concat([1, 2, 3, 4, 5, 6, 7, 8, 9])
+                    .concat([3, 1]))).toBe(false);
+        })
+
+        it('should find the number in column', () => {
+            let actualValue = numberInColumn(1, 0,
+                [2, 3, 4, 5, 6, 7, 8, 9, 1]
+                    .concat([1, 2, 3, 4, 5, 6, 7, 8, 9])
+                    .concat([3, 1])
+            );
+            expect(actualValue).toBe(true);
+        })
+
+        it('should ignore the indexed number in column', () => {
+            let actualValue = numberInColumn(2, 1, [1, 2, 3]);
+            expect(actualValue).toBe(false);
+        })
+    });
+
+    describe('3x3 grid check functionality', () => {
+        let numberInGrid = gameService.numberInGrid(3, 9);
+
+        it('should find the number', () => {
+
+        })
+        it("shouldn't find the number", () => {
+
+        })
+    });
+});
--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -4,46 +4,25 @@ import { PositionComponent } from "./position.component";
 @Injectable()
 export class GameService {
     generateNewPuzzle(arrayLength: number, numberRange: number): Array<number> {
-        let nextNumber = (existingNumber): number => {
-            if (!existingNumber) return 1;//Math.ceil(Math.random() * numberRange);
-            var newNumber = existingNumber < numberRange ? ++existingNumber : 1
-            return newNumber;
-        }
-        let numberInRow = (numberToTest, index, array): boolean => {
-            var row = Math.floor((index) / numberRange);
-            var numberInRowAlready = false;
-            for (let i = row * numberRange; i < (row * numberRange) + numberRange; i++) {
-                if (i == index) continue;
-                numberInRowAlready = (array[i] == numberToTest);
-                if (numberInRowAlready) break;
-            }
-            return numberInRowAlready;
-        };
-        let numberInColumn = (numberToTest, index, array): boolean => {
-            var column = index % numberRange;
-            var numberInColumnAlready = false;
-            for (let i = column; i < column + 73; i = i + numberRange) {
-                if (i == index) continue;
-                numberInColumnAlready = (array[i] == numberToTest);
-                if (numberInColumnAlready) break;
-            }
-            return numberInColumnAlready;
-        };
+        let nextNumber = this.nextNumber(numberRange)
+        let numberInRow = this.numberInRow(numberRange);
+        let numberInColumn = this.numberInColumn(numberRange);
 
         console.log("Generate new puzzle");
-        var puzzleArray = Array.apply(null, new Array<number>(arrayLength));
+        var puzzleArray = Array.apply(null, new Array<object>(arrayLength)).map(o => new Object());
         for (let i = 0; i < puzzleArray.length; i++) {
-            var retries = 0;
+            var retries = puzzleArray[i].retries || 0;
             while (retries < 10) {
                 retries++;
-                var positionValue = puzzleArray[i];
+                var positionValue = puzzleArray[i].value;
                 var numberToTest = nextNumber(positionValue);
-                puzzleArray[i] = numberToTest;
-                if (numberInRow(numberToTest, i, puzzleArray)) {
+                puzzleArray[i].value = numberToTest;
+                puzzleArray[i].retries = retries;
+                if (numberInRow(numberToTest, i, puzzleArray.map(po => po.value))) {
                     // try another number
                     continue;
                 }
-                if (numberInColumn(numberToTest, i, puzzleArray)) {
+                if (numberInColumn(numberToTest, i, puzzleArray.map(po => po.value))) {
                     // try another number
                     continue;
                 }
@@ -51,8 +30,55 @@ export class GameService {
             }
             if (retries == 10) i = i - 2;  // change previous number to another valid value to free up another valid option for this position
         }
-        return puzzleArray;
+        return puzzleArray.map(po => po);
     }
+
+    numberInColumn(numberRange: number) {
+        return (numberToTest, index, array): boolean => {
+            var column = index % numberRange;
+            var numberInColumnAlready = false;
+            for (let i = column; i < column + 73; i = i + numberRange) {
+                if (i == index)
+                    continue;
+                numberInColumnAlready = (array[i] == numberToTest);
+                if (numberInColumnAlready)
+                    break;
+            }
+            return numberInColumnAlready;
+        };
+    }
+
+    numberInRow(numberRange: number) {
+        return (numberToTest: number, index: number, array: Array<number>): boolean => {
+            var row = Math.floor((index) / numberRange);
+            var numberInRowAlready = false;
+            for (let i = row * numberRange; i < (row * numberRange) + numberRange; i++) {
+                if (i == index)
+                    continue;
+                numberInRowAlready = (array[i] == numberToTest);
+                if (numberInRowAlready)
+                    break;
+            }
+            return numberInRowAlready;
+        };
+    }
+
+    nextNumber(numberRange: number) {
+        return (existingNumber): number => {
+            if (!existingNumber)
+                return 1; //Math.ceil(Math.random() * numberRange);
+            var newNumber = existingNumber < numberRange ? ++existingNumber : 1;
+            return newNumber;
+        };
+    }
+
+    numberInGrid(gridSize: number, numberRange: number) {
+        return (numberToTest: number, index: number, array: Array<number>): boolean => {
+            var numberFound = false;
+            return numberFound;
+        };
+    }
+
     setSelectedPositionValue(positionValue: number) {
         this.selectedPosition.value = positionValue;
         console.log("Changed position value: " + positionValue);
